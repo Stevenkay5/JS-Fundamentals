35b50c801b40707405ef39a4448a3f2c9cd207e2
Steven Kayiranga <stevenkayiranga5@gmail.com>
2025-11-09 23:46:18 +0200

    Add

---
+++ 9-add.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+    return a + b;
+}
+const num1= parseInt(process.argv[2]);
+const num2= parseInt(process.argv[3]);
+if (isNaN(num1) || isNaN(num2)) {
+    console.log("Please provide two valid integers as arguments");
+} else {
+    const result = add(num1, num2);
+    console.log(result);
+}
